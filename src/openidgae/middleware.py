ca19f0b1dd4f360eb10b294e2575af7c71fb23f0
wtanaka@yahoo.com <wtanaka@yahoo.com@aab640e6-a80e-11dd-8af2-493fb0bb9f7c>
2009-06-17 13:28:30 +0000

    Add X-XRDS-Location on root URL for OpenID 2.0
    see:
    http://developer.yahoo.com/openid/faq.html
    http://openid.net/specs/openid-authentication-2_0.html#rp_discovery
    
    git-svn-id: https://google-app-engine-django-openid.googlecode.com/svn/trunk@39 aab640e6-a80e-11dd-8af2-493fb0bb9f7c

--- src/openidgae/middleware.py
+++ src/openidgae/middleware.py
@@ -33,3 +33,20 @@ class OpenIDMiddleware(object):
       import logging
       logging.info(message)
     oidutil.log = myLoggingFunction
+
+  def process_response(self, request, response):
+    # Yahoo wants to be able to verify the location of a Relying
+    # Party's OpenID 2.0 endpoints using Yadis
+    # http://developer.yahoo.com/openid/faq.html
+    # so we need to publish our XRDS file on our realm URL.  The Realm
+    # URL is specified in OpenIDStartSubmit as the protocol and domain
+    # name of the URL, so we check if this request is for the root
+    # document there and add the appropriate header if it is.
+    if request.path == '/':
+      import django.core.urlresolvers
+      response['X-XRDS-Location'] = ''.join((
+          'http', ('', 's')[request.is_secure()], '://',
+          request.META['HTTP_HOST'],
+          django.core.urlresolvers.reverse('openidgae.views.RelyingPartyXRDS')
+          ))
+    return response
